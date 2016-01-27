61065d963f7ee05120ca61b22ef412f05985fe4e
jss87230 <jss87230@ucmo.edu>
2016-01-27 15:31:12 -0600

    Node Class Finished

--- Room Editor/Room Editor/Node.cs
+++ Room Editor/Room Editor/Node.cs
@@ -9,17 +9,20 @@ namespace Room_Editor
     class Node
     {
         private float x, y;
-        private bool taken = false;
+        private string name;
         public List<Node> connections = new List<Node>();
+        public List<double> weights = new List<double>();
 
-        public Node(float inX, float inY)
+        public Node(string name, float inX, float inY)
         {
+            this.name = name;
             x = inX;
             y = inY;
         }
 
         public void addConnection(Node a){
             connections.Add(a);
+            weights.Add(Math.Sqrt(Math.Pow(a.x - x, 2) + Math.Pow(a.y - y, 2)));
         }
     }
 }
