1f54e5f2944d6b79ee63c26e4d1adf6752d37e87
jss87230 <jss87230@ucmo.edu>
2016-02-17 16:07:48 -0600

    Start new Project
    Started Dyksra's algorithm
    Also changes to the Node Class

---
+++ Testing_Ground_Area_51/Testing_Ground_Area_51/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_Ground_Area_51 {
+    class Person {
+
+        public int x, y;
+        public Node currentNode { get; set; }
+
+        public string Destination { get; set; }
+        public List<Node> path = new List<Node>();
+
+        public Person(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void calculatePath(Node dest) {
+            currentNode.resetPathfinding();
+
+            currentNode.totalDistance = 0;
+            do {
+                Node neighbor = currentNode.getSmallestDistance();
+
+                List<Node> connections = neighbor.connections;
+
+                foreach(Node n in connections) {
+
+                }
+            } while(true);
+        }
+    }
+}
